84d7e1c9c9ad5d7e088a315fd5783e8e1bcf757c
martin jimenez <majiar16@gmail.com>
2020-11-17 15:40:22 -0500

    begin project

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+
+app.set('port', process.env.PORT || 3000);
+
+const mongoDB = process.env.MONGO_URI;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+
+db.on('error', err =>{
+    console.log("conexion con errores ", err);
+});
+db.once('open', () =>{
+    console.log("conexion exitosa");
+})
+
+
+
+app.listen(app.get('port'), ()=>{
+    console.log("server corriendo");
+});

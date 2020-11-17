dd7f55feeccfa98a52edc2c6c48abe3edaaea9a8
martin jimenez <majiar16@gmail.com>
2020-11-17 17:37:57 -0500

    end project

--- controller/epsController.js
+++ controller/epsController.js
@@ -5,9 +5,9 @@ exports.getAllEpsNoCovid = function(req, res){
         const {eps} = req.params;
         epsModel.find({eps: eps, atiendeCovid:false}, (err, Findeps) =>{
             if(!Findeps){
-                res.status(300).send("no se encontraron resultado que coincidan con ", eps);
+                res.status(300).send("no se encontraron resultado que coincidan con " + eps);
             }else if(err){
-                res.status(400).send("hubo un error en la peticion ", err);
+                res.status(400).send("hubo un error en la peticion " + err);
             }else {
                 res.status(200).send(Findeps);
             }
@@ -22,9 +22,9 @@ exports.getAllEpsCovid = function(req, res){
         const {eps} = req.params;
         epsModel.find({eps: eps, atiendeCovid:true}, (err, Findeps) =>{
             if(!Findeps){
-                res.status(300).send("no se encontraron resultado que coincidan con ", eps);
+                res.status(300).send("no se encontraron resultado que coincidan con " + eps);
             }else if(err){
-                res.status(400).send("hubo un error en la peticion ", err);
+                res.status(400).send("hubo un error en la peticion " + err);
             }else {
                 res.status(200).send(Findeps);
             }
@@ -37,18 +37,17 @@ exports.getAllEpsCovid = function(req, res){
 exports.getOneEps = function(req, res) {
     if(req.params.id){
         const {id} = req.params;
-        epsModel.findOne({_id: id}, (err, Findeps) =>{
+        epsModel.find({_id: id}, (err, Findeps) =>{
             if(!Findeps){
-                res.status(300).send("no se encontraron resultado que coincidan con ", eps);
+                res.status(400).send("no se encontraron resultado que coincidan con " + id);
             }else if(err){
-                res.status(400).send("hubo un error en la peticion ", err);
+                res.status(400).send("hubo un error en la peticion " + err);
             }else {
-                console.log("object");
                 res.status(200).send(Findeps);
             }
         });
     }else{
-        res.status(400).send("no se mando la id de la eps");
+        res.status(400).send("no se mando el id de eps")
 
     }
 }
